18a4cbb68b457acd23bf3bacb894c2c087e5b41e
Aryan <aryankawale163@gmail.com>
2025-12-31 23:24:15 +0530

    Fix: Add backend files

---
+++ backend/src/app.js
@@ -0,0 +1,24 @@
+import express from "express"
+import cors from "cors"
+const app = express()
+// import cookieParser from "cookie-parser"
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN
+}))
+
+// app.use(express.json({ limit: "16kb" }))
+// app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+// app.use(express.static("public"))
+// app.use(cookieParser())
+
+
+// routes
+
+// import userRouter from "./routes/user.routes.js"
+
+// app.use("/api/v1/user", userRouter)
+
+
+
+export { app }
